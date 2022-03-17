760818c08c1d1bc7d8641fd9336f306defc59bb8
olwenn <lokto@hotmail.es>
2022-03-17 09:08:48 +0100

    Agregar archivos de proyecto.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+        public static void SayGoodBye()
+        {
+            Console.WriteLine("Bye bro!");
+
+        }
+    }
+}
